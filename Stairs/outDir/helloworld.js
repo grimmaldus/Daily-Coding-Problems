6a313397d878f0f1e1c27d4d85850a76b09f6827
Daniel Bendik <daniel.bendik@accenture.com>
2019-07-12 15:00:38 +0200

    init + stairs coding problem

---
+++ Stairs/outDir/helloworld.js
@@ -0,0 +1,9 @@
+function stairs(n) {
+    if (n <= 1)
+        return 1;
+    return stairs(n - 1) + stairs(n - 2);
+}
+function stairsSolution() {
+    console.log(stairs(4));
+}
+//# sourceMappingURL=helloworld.js.map
