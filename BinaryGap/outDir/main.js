2060b983c0af5e096ba02c892a9d29715496088f
Daniel Bendik <daniel.bendik@accenture.com>
2019-08-12 11:13:43 +0200

    change binaryGap solution

--- BinaryGap/outDir/main.js
+++ BinaryGap/outDir/main.js
@@ -1,24 +1,23 @@
 // A binary gap within a positive integer N is any maximal sequence of 
 // consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
-function solution(N: number): number { 
-    const binary = N.toString(2); console.log(binary);
-    let maxGap = 0;
-    let currentGap = 0;
-
-    for(let i = 0; i < binary.length; i++)
-    {
+function solution(N) {
+    var binary = N.toString(2);
+    console.log(binary);
+    var maxGap = 0;
+    var currentGap = 0;
+    for (var i = 0; i < binary.length; i++) {
         // trailing 0
-        if(binary[i] === '0') continue;
-
+        if (binary[i] === '0')
+            continue;
         // begin of gap
-        if(binary[i] === '1') {
+        if (binary[i] === '1') {
             // get size of gap
             currentGap = 0;
-            i++
-            while( binary[i] === '0' ) {
+            i++;
+            while (binary[i] === '0') {
                 i++;
-                if ( i >= binary.length ){
-                    console.log(0);    
+                if (i >= binary.length) {
+                    console.log(0);
                     return 0;
                 }
                 currentGap++;
@@ -30,6 +29,6 @@ function solution(N: number): number {
     console.log(maxGap);
     return maxGap;
 }
-
 //test
 solution(95);
+//# sourceMappingURL=main.js.map
